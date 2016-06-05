817d4c778aa8be88b9760277f399d1eb077936ff
Stephen A. Collins-Elliott <sce@utk.edu>
2016-06-05 01:18:16 -0400

    move.py

---
+++ move.py
@@ -0,0 +1,15 @@
+infsp = int(input("Inf SP "))
+cavsp = int(input("Cav SP "))
+elesp = int(input("Ele SP "))
+cavspadj = 2 * cavsp
+adjsptotal = infsp + cavspadj + elesp
+
+approv = int(input("AP prov total")) #road #devast 
+apmoun = 6 * int(input("# mountains"))
+apmars = 3 * int(input("# marshes"))
+aprivemi = int(input("# minor river crossings"))
+aprivemo = 2 * int(input("# moderate river crossings"))
+aprivema = 3 * int(input("# major river crossings"))
+
+
+accap
