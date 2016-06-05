e6bfb218149d3c929f467b18cff5b61eb3ed3761
Stephen A. Collins-Elliott <sce@utk.edu>
2016-06-05 01:42:28 -0400

    Update move.py

--- move.py
+++ move.py
@@ -2,14 +2,40 @@ infsp = int(input("Inf SP "))
 cavsp = int(input("Cav SP "))
 elesp = int(input("Ele SP "))
 cavspadj = 2 * cavsp
-adjsptotal = infsp + cavspadj + elesp
+sp = infsp + cavsp + elesp
+adjsp = infsp + cavspadj + elesp
 
-approv = int(input("AP prov total")) #road #devast 
+approv = int(input("AP province total")) #road #devast 
 apmoun = 6 * int(input("# mountains"))
 apmars = 3 * int(input("# marshes"))
 aprivemi = int(input("# minor river crossings"))
 aprivemo = 2 * int(input("# moderate river crossings"))
 aprivema = 3 * int(input("# major river crossings"))
 
+accap = approv + apmoun + apmars + aprivemi + aprivemo + aprivema
 
-accap
+alp = 0
+
+if 1 <= adjsp <= 10:
+    if 1 <= accap <= 16:
+        alp = 0
+    if 17 <= accap <= 42:
+        if sp == 1:
+            alp = 0
+        else:
+            alp = 1
+    if 43 <= accap:
+        alp = 2
+if 11 <= adjsp <= 25:
+    if 1 <= accap <= 6:
+        alp = 0
+    if 7 <= accap <= 21:
+        alp = 1
+    if 22 <= accap <= 27:
+        alp = 2
+    if 28 <= accap <= 34:
+        alp = 3
+    if 35 <= accap <= 42:
+        alp = 4
+    if 43 <= accap:
+        alp = 5
